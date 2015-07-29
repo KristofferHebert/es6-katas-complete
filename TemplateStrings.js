a27c891b6a9edd454d2b15359190b0f51b8fde93
= <kristoffer@kristofferhebert.com>
2015-07-28 20:37:13 -0700

    Fixing spacing TemplateString.js

--- TemplateStrings.js
+++ TemplateStrings.js
@@ -6,17 +6,17 @@ describe('template string, are wrapped in backticks', function() {
 	it('prints x into a string using ${x}', function() {
 		var x = 42;
 		assert.equal(`x=${x}`, 'x=' + x);
-	    });
+	});
   
 	it('add two numbers inside the ${...}', function() {
 		var x = 42;
 		var y = 23;
 		assert.equal(`${ x + y }`, x+y);
-	    });
+	});
   
 	it('get a function call result inside ${...}', function() {
 		function getDomain(){ return document.domain }
 		assert.equal(`${ getDomain() }`, 'tddbin.com');
-	    });
+	});
   
 });
